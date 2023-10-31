93529b1f36667c9eeb80f96a6632b4187aea752a
Sebastian Jurado <sjurado@ufl.edu>
2023-10-31 18:40:20 -0400

    revised main function and decoder function

--- cypher.py
+++ cypher.py
@@ -8,17 +8,34 @@ def encoder(nums):
             out.append(nums[i] + 3)
     return out
 
+def decoder(encoded_nums):
+    decoded_password = ''
+    for i in encoded_nums:
+        digit = int(i)
+        if digit < 3:
+            decoded_password += str(digit + 7)
+        else:
+            decoded_password += str(digit - 3)
+    return decoded_password
+
+
 def main():
-    option = 0
-    while option != 3:
-        input("Menu\n-------------\n1.Encode\n2. Decode\n3. Quit")
+    encoded_password = None  # To store the encoded password for decoding later
+    while True:
+        print("Menu\n-------------\n1.Encode\n2.Decode\n3.Quit")
+        option = int(input("Please enter your choice: "))
         if option == 1:
-            nums = input("Please enter your password to encode:")
-            encoder(nums)
-            print("Your password has been encoded and stored!")
-        if option == 2:
-            decoder(nums)
-            print("Your password has been encoded and stored!")
+            nums = input("Please enter your password to encode (only numbers): ")
+            encoded_password = encoder(nums)
+            print(f"Your password has been encoded: {encoded_password}")
+        elif option == 2:
+            if encoded_password:
+                decoded = decoder(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {decoded}.")
+            else:
+                print("There's no encoded password to decode.")
+        elif option == 3:
+            break
 
 if __name__ == "__main__":
     main()
